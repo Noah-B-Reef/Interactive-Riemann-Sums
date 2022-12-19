4ac90572c6ad6793040d02d2844786c909de2a5d
Noah Reef <n.reef23@gmail.com>
2022-12-18 22:45:58 -0800

    Initial Commit

---
+++ riemann.py
@@ -0,0 +1,14 @@
+class Riemann:
+
+   # Constructor
+    def __init__(self, func, stepSize, interval, numOfSteps):
+        self.func = func
+        self.stepSize = stepSize
+        self.interval = interval
+        self.numOfSteps = numOfSteps
+
+    def eval(self):
+        pass
+
+    def draw(self):
+        pass
